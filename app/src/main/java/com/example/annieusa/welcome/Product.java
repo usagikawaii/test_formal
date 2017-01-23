aee16972caa5d39638ae4e74a9bcf576244bb82b
usagikawaii <annie.liao0108@gmail.com>
2017-01-24 00:09:29 +0800

    git remote add origin https://github.com/usagikawaii/test_formal.git git push -u origin master

--- app/src/main/java/com/example/annieusa/welcome/Product.java
+++ app/src/main/java/com/example/annieusa/welcome/Product.java
@@ -2,12 +2,38 @@ package com.example.annieusa.welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Product extends AppCompatActivity {
+
+    private Button button6;
+    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        button6 = (Button)findViewById(R.id.button6);
+        textView3 = (TextView)findViewById(R.id.textView3);
+
+        button6.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+
+            public void onClick(View v) {
+
+                // TODO Auto-generated method stub
+
+                textView3.setText(" ");
+
+            }
+
+        });
     }
+
 }
+
+
+
--- app/src/main/java/com/example/annieusa/welcome/Profile.java
+++ app/src/main/java/com/example/annieusa/welcome/Profile.java
@@ -2,12 +2,31 @@ package com.example.annieusa.welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    private Button button, button2;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        button = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+        textView = (TextView)findViewById(R.id.textView);
+
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                textView.setText("(已改變)此Activity是繼承BaseActivity");
+            }
+
+        });
     }
 }
+
