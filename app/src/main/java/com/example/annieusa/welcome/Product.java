1e32c49fab62fe7f73ed62f75aa3604ae19e6c1e
usagikawaii <annie.liao0108@gmail.com>
2017-01-24 00:36:20 +0800

    git remote add origin https://github.com/usagikawaii/test_formal.git git push -u origin master

--- app/src/main/java/com/example/annieusa/welcome/Product.java
+++ app/src/main/java/com/example/annieusa/welcome/Product.java
@@ -1,29 +1,40 @@
 package com.example.annieusa.welcome;
 
+import android.app.Dialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.content.DialogInterface;
 
 public class Product extends AppCompatActivity {
 
     private Button button6;
-    private TextView textView3;
+
+    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this).create();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
         button6 = (Button)findViewById(R.id.button6);
-        textView3 = (TextView)findViewById(R.id.textView3);
+
+        dialog.setTitle("基本訊息對話按鈕");
+        dialog.setMessage("基本訊息對話功能介紹");
+        dialog.setNeutralButton("OK",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                // TODO Auto-generated method stub
+                }
+        });
 
         button6.setOnClickListener(new Button.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-
+                dialog.show();
             }
 
         });
